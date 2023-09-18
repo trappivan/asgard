4d2a28bd2ab521d51154aa0d8d2ef8df94c209ad
ivan <ivan.trapp@univille.br>
2023-09-18 10:40:54 -0300

    aplicativo

---
+++ app/Login.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Box, Button, Input } from "@mui/material";
+
+export default function Login() {
+  return (
+    <Box component={"form"}>
+      <Input placeholder="login" />
+      <Input placeholder="senha" />
+      <Button variant="contained">Entrar</Button>
+      asdsadçççç
+    </Box>
+  );
+}
