01a083448e1cb922f949dabc7f3839543984eeec
ivan <ivan.trapp@univille.br>
2023-09-18 15:24:40 -0300

    algumas coisas

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { Box, Button, Input } from "@mui/material";
+import { sizing } from "@mui/system";
 
 export default function Login() {
-  return (
-    <Box component={"form"}>
-      <Input placeholder="login" />
-      <Input placeholder="senha" />
-      <Button variant="contained">Entrar</Button>
-      asdsadçççç
-    </Box>
-  );
+	return (
+		<Box
+			component={"form"}
+			sx={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+				height: "96vh",
+				gap: 4,
+			}}>
+			<Input placeholder="login" />
+			<Input placeholder="senha" />
+			<Button variant="contained">Entrar</Button>
+			asdsadçççç
+		</Box>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import { Box } from "@mui/material";
 import Login from "./Login";
 
 export default function Home() {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        border: 2,
-      }}
-    >
-      <Login />
-    </Box>
-  );
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				justifyContent: "center",
+				border: 2,
+				width: 1,
+				height: 1,
+			}}>
+			<Login />
+		</Box>
+	);
 }
